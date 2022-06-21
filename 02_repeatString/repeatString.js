54b8dd8f070ca0a693077d2ce09c189f1146efd5
Frenzy017 <aleksladjov4@gmail.com>
2022-06-21 20:18:27 +0300

    02_repeatString/repeatString.js solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function (str, n) {
 
-};
+    let string = str;
+    let counter = Number(n)
+    let result = []
+
+
+    for (let i = 0; i < counter; i++) {
+        result.push(string)
+    }
+
+    
+    if (counter >= 0) {
+        return result.join('')
+    } else {
+        return 'ERROR'
+    }
+
+
+}
+
+
+repeatString('hey', -1)
 
 // Do not edit below this line
 module.exports = repeatString;
